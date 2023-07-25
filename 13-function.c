fe4d919c7fe48ea6b4761447cb52c8f3a926f067
jamal <jamalimmiss97@gmail.com>
2023-07-25 14:52:31 -0700

    update

--- 13-function.c
+++ 13-function.c
@@ -10,14 +10,14 @@ int print_rev(va_list args)
 	char *s = va_arg(args, char *);
 	int l = 0, i, count = 0;
 
-	while (str[l])
+	while (s[l])
 	{
 		l++;
 	}
 
 	for (i = l - 1; i >= 0; i--)
 	{
-		_putchar(str[i]);
+		_putchar(s[i]);
 		count++;
 	}
 
